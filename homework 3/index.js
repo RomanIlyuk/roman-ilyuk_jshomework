20e39d7d5119f8ea47129b38569ccbe9c45fa6a8
Roman <ilyukroma228@gmail.com>
2020-10-25 12:27:40 +0200

    homework3

---
+++ homework 3/index.js
@@ -0,0 +1,13 @@
+const credits = 23580;
+const pricePerDroid = 3000;
+
+ let numberOfDroids = prompt ("Введіть кількість дроїдів");
+ let totalPrice = pricePerDroid * numberOfDroids;
+
+ if (numberOfDroids === null) {
+    console.log("Скасовано користувачем");
+} else if(totalPrice > credits) {
+    console.log("Недостатньо коштів на рахунку");
+} else {
+    console.log (`Ви купили ${numberOfDroids} дроїдів, на рахунку залишилось ${credits - totalPrice} кредитів.`);
+}
