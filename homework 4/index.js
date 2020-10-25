da699f77e3062d40768041e63caf21da2509cab9
Roman <ilyukroma228@gmail.com>
2020-10-25 12:30:32 +0200

    homework4

---
+++ homework 4/index.js
@@ -0,0 +1,13 @@
+let total = 0;
+let canInput = true;
+while(canInput){
+    const userInput = prompt('Введіть число');
+    if(userInput === null){
+        canInput = false; 
+       console.log(`Total: ${total}`);
+    }
+else {
+    const numberToAdd = parseInt(userInput);
+    total += numberToAdd;
+    }
+}
